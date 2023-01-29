c466b3e35db9e7ebca2789f37b5087634dcdef3d
Kayhan Aykin <kayhan.aykin@gmail.com>
2023-01-29 19:27:19 +0300

    bugfix #25

--- js/scriptallcourts.js
+++ js/scriptallcourts.js
@@ -3,7 +3,7 @@ var content ="";
 content += '<div id=courtstopcontainer>'
 
 for (i=0; i<4; i++) {
-    content += '<div class="court'+ (i+1) + '">' 
+    content += '<div class="courts">' 
     + '<div class="courtplayers1 courtplay">Roderick D.</div>'
     + '<div class="courtplayers2 courtplay">Devon M.</div>'
     + '<div class="courtred-box">12 min.</div>'
@@ -13,8 +13,17 @@ for (i=0; i<4; i++) {
 
 content += '</div><div id=courtsbottomcontainer>'
 
-for (i=4; i<8; i++) {
-    content += '<div class="court'+ (i+1) + '">' 
+for (i=4; i<6; i++) {
+    content += '<div class="courts">' 
+    + '<div class="courtplayers1 courtplay">Roderick D.</div>'
+    + '<div class="courtplayers2 courtplay">Devon M.</div>'
+    + '<div class="courtred-box">12 min.</div>'
+    + '<div class="courtplayers3 courtplay">Rick W.</div>'
+    + '<div class="courtplayers4 courtplay">Dori D.</div></div>';
+}
+
+for (i=6; i<8; i++) {
+    content += '<div class="courts inactive-court">' 
     + '<div class="courtplayers1 courtplay">Roderick D.</div>'
     + '<div class="courtplayers2 courtplay">Devon M.</div>'
     + '<div class="courtred-box">12 min.</div>'
