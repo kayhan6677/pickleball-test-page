9875a15c964be1d51187c70e3507884c1b152ce1
Kayhan Aykin <kayhan.aykin@gmail.com>
2023-01-30 20:56:47 +0300

    bugfix #31

--- js/scriptallcourts.js
+++ js/scriptallcourts.js
@@ -1,36 +1,36 @@
 var content ="";
 
-content += '<div id=courtstopcontainer>'
+content += '<div id=courts_top_container>'
 
 for (i=0; i<4; i++) {
     content += '<div class="courts">' 
-    + '<div class="courtplayers1 courtplay">Roderick D.</div>'
-    + '<div class="courtplayers2 courtplay">Devon M.</div>'
-    + '<div class="courtred-box">12 min.</div>'
-    + '<div class="courtplayers3 courtplay">Rick W.</div>'
-    + '<div class="courtplayers4 courtplay">Dori D.</div></div>';
+    + '<div class="court_players_1 court_play">Roderick D.</div>'
+    + '<div class="court_players_2 court_play">Devon M.</div>'
+    + '<div class="court_red-box">12 min.</div>'
+    + '<div class="court_players_3 court_play">Rick W.</div>'
+    + '<div class="court_players_4 court_play">Dori D.</div></div>';
 }
 
-content += '</div><div id=courtsbottomcontainer>'
+content += '</div><div id=courts_bottom_container>'
 
 for (i=4; i<6; i++) {
     content += '<div class="courts">' 
-    + '<div class="courtplayers1 courtplay">Roderick D.</div>'
-    + '<div class="courtplayers2 courtplay">Devon M.</div>'
-    + '<div class="courtred-box">12 min.</div>'
-    + '<div class="courtplayers3 courtplay">Rick W.</div>'
-    + '<div class="courtplayers4 courtplay">Dori D.</div></div>';
+    + '<div class="court_players_1 court_play">Roderick D.</div>'
+    + '<div class="court_players_2 court_play">Devon M.</div>'
+    + '<div class="court_red-box">12 min.</div>'
+    + '<div class="court_players_3 court_play">Rick W.</div>'
+    + '<div class="court_players_4 court_play">Dori D.</div></div>';
 }
 
 for (i=6; i<8; i++) {
     content += '<div class="courts inactive-court">' 
-    + '<div class="courtplayers1 courtplay">Roderick D.</div>'
-    + '<div class="courtplayers2 courtplay">Devon M.</div>'
-    + '<div class="courtred-box">12 min.</div>'
-    + '<div class="courtplayers3 courtplay">Rick W.</div>'
-    + '<div class="courtplayers4 courtplay">Dori D.</div></div>';
+    + '<div class="court_players_1 court_play">Roderick D.</div>'
+    + '<div class="court_players_2 court_play">Devon M.</div>'
+    + '<div class="court_red-box">12 min.</div>'
+    + '<div class="court_players_3 court_play">Rick W.</div>'
+    + '<div class="court_players_4 court_play">Dori D.</div></div>';
 }
 
 content += '</div>'
 
-document.getElementById("allcourtscontainer").innerHTML += content;
+document.getElementById("all_courts_container").innerHTML += content;
--- js/scriptwaitinglist.js
+++ js/scriptwaitinglist.js
@@ -1,11 +1,11 @@
-const gametype = ["D", "D","D", "D", "D", "D", "D", "S"];
-const gamerating = [3.5, 3.5, "handshake", 3.5,"handshake", "handshake", "handshake", "handshake"];
-const player1 = ["Lynn Tanner", "Roderick Flich", "Angus McGyver", "Hannibal Smith", "Thomas Magnum", "Roderick Decker",
+const game_type = ["D", "D","D", "D", "D", "D", "D", "S"];
+const game_rating = [3.5, 3.5, "handshake", 3.5,"handshake", "handshake", "handshake", "handshake"];
+const player_1 = ["Lynn Tanner", "Roderick Flich", "Angus McGyver", "Hannibal Smith", "Thomas Magnum", "Roderick Decker",
 "Cristian Tanner", "B.A. Baracus", "Lynn Tanner", "Roderick Flich", "Angus McGyver", "Hannibal Smith", "Thomas Magnum",
 "Roderick Decker", "Cristian Tanner"];
-const player2 = ["Sledge Hammer", "Willie Tanner", "Micheal Knight", "Fred Decker", "Tony Danza", "Your Name", "Capt. Trunk",
+const player_2 = ["Sledge Hammer", "Willie Tanner", "Micheal Knight", "Fred Decker", "Tony Danza", "Your Name", "Capt. Trunk",
 "Dori Doreau", "Sledge Hammer", "Willie Tanner", "Micheal Knight", "Fred Decker", "Tony Danza", "Your Name", "Capt. Trunk"];
-const waitingtime = [16, 19, 21, 26, 19, 19, 21, 26];
+const waiting_time = [16, 19, 21, 26, 19, 19, 21, 26];
 
 var content ="";
 
@@ -16,34 +16,34 @@ var content ="";
             content += '<div class="row extended">';
         }
 
-        if (gametype[i]=="D") {
-        content += '<div class="gametype"><img src="img/double.svg" alt="Double Symbol"></div>';
+        if (game_type[i]=="D") {
+        content += '<div class="game_type"><img src="img/double.svg" alt="Double Symbol"></div>';
         } else {
-        content += '<div class="gametype"><img src="img/single.svg" alt="Single Symbol"></div>';
+        content += '<div class="game_type"><img src="img/single.svg" alt="Single Symbol"></div>';
         } 
 
-        if (gamerating[i]=="3.5") {
-            content += '<div class="gamerating">3.5</div>';
+        if (game_rating[i]=="3.5") {
+            content += '<div class="game_rating">3.5</div>';
         } else {
-            content +='<div class="gamerating"><img class="handshake" src="img/handshake.svg" alt="Handshake"></div>';
+            content +='<div class="game_rating"><img class="hand_shake" src="img/handshake.svg" alt="Handshake"></div>';
         }
-        if (gametype[i]=="D") {
+        if (game_type[i]=="D") {
             if (i==2) {
-                content += '<div class="players1 current-user-game">'+player1[2*i]+' <br> '+player1[2*i+1]+ '</div>'
-            + '<div class="players2 current-user-game">' + player2[2*i] + ' <br><span class="current-user"> ' + player2[2*i+1] + '</span></div>';
+                content += '<div class="players_1 current-user-game">'+player_1[2*i]+' <br> '+player_1[2*i+1]+ '</div>'
+            + '<div class="players_2 current-user-game">' + player_2[2*i] + ' <br><span class="current-user"> ' + player_2[2*i+1] + '</span></div>';
             } else { 
-            content += '<div class="players1">'+player1[2*i]+' <br> '+player1[2*i+1]+ '</div>'
-            + '<div class="players2">' + player2[2*i] + ' <br> ' + player2[2*i+1] + '</div>';
+            content += '<div class="players_1">'+player_1[2*i]+' <br> '+player_1[2*i+1]+ '</div>'
+            + '<div class="players_2">' + player_2[2*i] + ' <br> ' + player_2[2*i+1] + '</div>';
             }
         } else {
-        content += '<div class="players1">'+ player1[2*i] +'</div>'
-        + '<div class="players2">' + player2[2*i] + '</div>';  
+        content += '<div class="players_1">'+ player_1[2*i] +'</div>'
+        + '<div class="players_2">' + player_2[2*i] + '</div>';  
         }
         
-        content += '<div class="waitingtime"><span class="waitingtimeminutes">' + waitingtime[i] + ' </span> mins</div>'
-        + '<div class="trashcan"><img src="img/trashcan.svg" alt="Trash Can"></div>'
+        content += '<div class="waiting_time"><span class="waiting_time_minutes">' + waiting_time[i] + ' </span> mins</div>'
+        + '<div class="trash_can"><img src="img/trashcan.svg" alt="Trash Can"></div>'
         + '</div>';
        
     }
 
-    document.getElementById("waitinglistbody").innerHTML += content;
+    document.getElementById("waiting_list_body").innerHTML += content;
